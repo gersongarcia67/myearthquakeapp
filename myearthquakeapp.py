5e56f084bae283ea98e297832e4e6d86e98ebd3b
Gerson Garcia <gerson@mydevpi.gegarcia.home>
2018-06-05 22:28:50 -0700

    added time

--- myearthquakeapp.py
+++ myearthquakeapp.py
@@ -2,8 +2,12 @@
 
 import requests
 import json
+from time import gmtime, strftime, localtime
+from datetime import datetime
 
-url='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02'
+
+
+url='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-06-05'
 
 response=requests.get(url)
 
@@ -11,9 +15,16 @@ if response.ok:
 
     jData=json.loads(response.content)
 
-    for key in jData:
+    for feature in jData['features']:
         print '==== START ===='
-        print key+":"+str(jData[key])
+        print feature['properties']['detail']
+        print feature['properties']['place']
+        print str(feature['properties']['mag'])
+        print str(feature['geometry']['coordinates'])
+        eqtime=feature['properties']['time']
+        print str(eqtime)
+        ms=eqtime/1000.0
+        print str(datetime.utcfromtimestamp(ms))
         print '==== END ===='
 
 else:
